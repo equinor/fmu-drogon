2cd3b8a2342f8d234be7fdf55d23f80144f05f7e
Trine Alsos (CCN GGP GGP8) <tral@st-lintgx0288.st.statoil.no>
2024-12-03 11:10:30 +0100

    replaced multi pem with more efficent version

--- sim2seis/bin/run_multi_pem.py
+++ sim2seis/bin/run_multi_pem.py
@@ -1,23 +1,22 @@
 #! /usr/bin/env python
 """
 This script executes Petro-Elastic Modelling using the petro_elastic library
-available on Komodo.
+available on Komodo
 
 Author: Jimmy Zurcher (jiz@equinor.com), November 2020
-Updated April 2021 to use several PEM config depending a discrete parameter.
-Vegard Berg 28.02.2022 - multi PEM use
 """
-import os
+import time
+from os import getenv, remove
+
 import pandas as pd
+import open_petro_elastic as pem
 from open_petro_elastic.__main__ import (
     make_input,
     load_data_from_csv,
 )
-import open_petro_elastic as pem
 import fmu.config.utilities as utils
-from os import getenv
 
-CFG = utils.yaml_load("../../fmuconfig/output/global_variables.yml")
+
 # Read environment variable
 SIM2SEIS_PRED = getenv("FLOWSIM_IS_PREDICTION")
 print(f"SIM2SEIS_PRED is {SIM2SEIS_PRED}")
@@ -39,46 +38,28 @@ else:
     ]
     print(SEISDATES)
 
+PROPS_DYNAMIC = ["PRESSURE", "RS", "SWAT", "SGAS"]
+PROPS_STATIC = ["FSAND", "PORO", "dZ", "PRESSURE_INIT", "Zone"]
 
-PROPS_DATE_NAME = ["PRESSURE", "RS", "SWAT", "SGAS"]
+PEM_CSV_INPUT = "../../sim2seis/input/pem/pem_data_input.csv"
+PEM_CSV_RESULTS = "../../sim2seis/output/pem/pem_results.csv"
 
-PEM_CSV = "../../sim2seis/input/pem/pem_data_input.csv"
-
-RUN_ID = ""
-
-MULTI_PEM_CONFIG = [
+PEM_CONFIG_FILES = [
     {
         "parameter": "Zone",
         "values": "1,3",
-        "PEM config": "../../sim2seis/model/pem/pem_config_valysar_volon.yml",
+        "PEM_config": "../../sim2seis/model/pem/pem_config_valysar_volon.yml",
     },
     {
         "parameter": "Zone",
         "values": "2",
-        "PEM config": "../../sim2seis/model/pem/pem_config_therys.yml",
+        "PEM_config": "../../sim2seis/model/pem/pem_config_therys.yml",
     },
-
 ]
+RUN_ID = ""
+
 KEEP_MISSING_ROWS = True
 UNDEF_VALUES = None
-
-
-def check_config(config, data):
-    assert isinstance(config, list), "Config should be a list of dict!"
-    for num, subset in enumerate(config):
-        assert isinstance(subset, dict), f"Subset {num} should be a dict!"
-        check = all(
-            item in subset.keys() for item in ["parameter", "values", "PEM config"]
-        )
-        message = (
-            f"Subset {num} should contain keys 'parameter', 'values' and 'PEM config'!"
-        )
-        assert check, message
-        message = f"Subset {num} has invalid parameter {subset['parameter']}!"
-        assert subset["parameter"] in data.columns, message
-        message = f"Subset {num} has invalid PEM config {subset['PEM config']}!"
-        assert os.path.isfile(subset["PEM config"]), message
-
 
 def get_values_list(subset_number, subset_values):
     values_list = []
@@ -110,7 +91,6 @@ def get_values_list(subset_number, subset_values):
             raise ValueError(error_message)
     return values_list
 
-
 def make_sub_dataframe(dataframe, subset_dict, subset_number):
     param = subset_dict["parameter"]
     values_list = get_values_list(subset_number, subset_dict["values"])
@@ -118,155 +98,106 @@ def make_sub_dataframe(dataframe, subset_dict, subset_number):
     return subset_df
 
 
-def check_coverage(my_config, input_df):
-    coverage = True
-    coverage_list = []
-    for i_subset, subset in enumerate(my_config):
-        param = subset["parameter"]
-        values = get_values_list(i_subset, subset["values"])
-        # Compute corresponding indexes from input_df for param and values
-        indexes = input_df.loc[input_df[param].isin(values)].index.tolist()
-        # Add indexes to a list
-        coverage_list.extend(indexes)
-    # Check for multiples in coverage_list (--> overlap)
-    if len(coverage_list) != len(set(coverage_list)):
-        raise ValueError("ERROR: Overlap between different subsets!")
-    # Compare list and input_df.index (--> missing values)
-    if len(coverage_list) < len(input_df.index.tolist()):
-        print("WARNING: some input data are not covered by the sum of subsets!")
-        coverage = False
-    return coverage
+def run_pem(pem_configs):
 
+    t_start = time.perf_counter()
 
-def add_missing_rows(data, results, res_col, undef_value):
-    all_indexes = data.index
-    res_indexes = results.index
-    missing_indexes = list(set(all_indexes) - set(res_indexes))
-    empty_res = pd.DataFrame(index=missing_indexes, columns=res_col)
-    if undef_value is not None:
-        empty_res.fillna(value=undef_value, inplace=True)
-    empty_data = data.iloc[missing_indexes]
-    subset_missing = pd.concat([empty_data, empty_res], axis=1)
-    return results.append(subset_missing)
+    df_ecl = pd.read_csv(PEM_CSV_INPUT)
+    df_static = df_ecl[PROPS_STATIC].copy()
 
+    print("start looping over dates")
 
-def run_multi_config_pem(
-    config, data_file, date, keep_missing_rows=True, undef_value=None
-):
-    """Run multiple (PEM configurations / data subsets) at a given date
-
-    Args:
-        config: a list of dictionaries corresponding to each subset of data.
-        data_file: the relative path of the input data file as csv file.
-        date: a String with the date of modelling (in case of 4D modelling).
-        keep_missing_rows: in case the sum of subsets of data in config does
-            not cover the entire span of input data, the rows of data not
-            covered will be kept if keep_missing_rows is True, those rows
-            will be removed from the output file otherwise.
-        undef_value: the value to assign to the output rows if no coverage
-            and keep_missing_rows is True.
-
-    """
-    # Load input data
-    data = load_data_from_csv(data_file)
-
-    # Check config
-    check_config(config, data)
-
-    # Check overlap or missing coverage
-    cov = check_coverage(config, data)
-
-    # Launch calculations
-    results = pd.DataFrame()
-    n_subsets = len(config)
+    n_subsets = len(pem_configs)
     print(f">> {n_subsets} subset(s) identified...")
 
-    for i, subset in enumerate(config):
-
-        print(f">> Subset {i+1}/{n_subsets}: {subset['PEM config']}...")
-
-        # Make subset of the input
-        subset_data = make_sub_dataframe(data, subset, i)
-        subset_data_file = "input_subset_" + str(i) + ".csv"
-        subset_data.to_csv(subset_data_file)
-
-        # Define inputs
-        inp = make_input(subset["PEM config"], subset_data_file)
-
-        # Compute PEM results
-        mixed_mineral = inp.minerals.as_mixture
-        mixed_fluid = inp.fluids.as_mixture(inp.pressure)
-        dry_rock_material = inp.dry_rock.material(mixed_mineral, inp.pressure)
-        dense_packing = inp.dry_rock.nonporous(mixed_mineral)
-        saturated_rock = pem.material.fluid_substitution(
-            dry_rock_material,
-            dense_packing,
-            mixed_fluid,
-            inp.dry_rock.porosity,
-        )
-
-        # Compute DT properties and make DataFrame with results
-        dz = subset_data["dZ"]
-        vp = saturated_rock.primary_velocity
-        vs = saturated_rock.secondary_velocity
-        dtpp = 2000.0 * dz / vp
-        dtss = 2000.0 * dz / vs
-        dtps = (dtpp + dtss) / 2.0
-
-        res = pd.DataFrame(
-            {
-                "VP_" + date + RUN_ID: vp,
-                "VS_" + date + RUN_ID: vs,
-                "AI_" + date + RUN_ID: saturated_rock.acoustic_impedance,
-                "SI_" + date + RUN_ID: saturated_rock.shear_impedance,
-                "DENS_" + date + RUN_ID: saturated_rock.density,
-                "VPVS_" + date + RUN_ID: vp / vs,
-                "DTPP_" + date + RUN_ID: dtpp,
-                "DTSS_" + date + RUN_ID: dtss,
-                "DTPS_" + date + RUN_ID: dtps,
-            },
-            index=subset_data.index,
-        )
-        # Add PEM results to the inputs and merge it with "results"
-        subset_results = pd.concat([subset_data, res], axis=1)
-        results = pd.concat([results,subset_results])
-
-        # Delete csv file for subset data
-        os.remove(subset_data_file)
-
-    # Manage data points not covered by config
-    if not cov and keep_missing_rows:
-        results = add_missing_rows(data, results, res.columns, undef_value)
-
-    # Re-order the results in increasing indexes to match the data format
-    results.sort_index(inplace=True)
-    # If index = True, keep the indexing (for QC)
-    # should be False to import results back to RMS
-    results.to_csv(data_file, index=False)
-
-
-def run_pem():
+    dfs = []
     for date in SEISDATES:
-        date = str(date).replace("-", "")
-        print("Date: " + date)
 
-        # Create new input names using generic names for dynamic properties
-        dframe = load_data_from_csv(PEM_CSV)
-        for pname in PROPS_DATE_NAME:
-            dframe[pname] = dframe[pname + "_" + date]
-        dframe.to_csv(PEM_CSV, index=False)
+        print(date)
+        df_date = df_static.copy()
 
-        # Run PEM on every subset defined in MULTI_PEM_CONFIG for given date
-        run_multi_config_pem(
-            MULTI_PEM_CONFIG,
-            PEM_CSV,
-            date,
-            keep_missing_rows=KEEP_MISSING_ROWS,
-            undef_value=UNDEF_VALUES,
-        )
-    print(f"Results saved as <{PEM_CSV}>")
+        for pname in PROPS_DYNAMIC:
+            df_date[pname] = df_ecl[pname + "_" + date]
+
+        df_date.to_csv(PEM_CSV_RESULTS, index=False)
+
+        results = pd.DataFrame()
+
+        # Loop through different PEM configs for different zones
+        for i, subset in enumerate(pem_configs):
+
+            print(f">> Subset {i+1}/{n_subsets}: {subset['PEM_config']}...")
+
+            # Make subset of the input
+            subset_data = make_sub_dataframe(df_date, subset, i)
+            subset_data_file = "input_subset_" + str(i) + ".csv"
+            subset_data.to_csv(subset_data_file)
+
+            # Define inputs
+            config_file=subset["PEM_config"]
+            with open(config_file, "r") as f:
+                pem_input = make_input(f, subset_data_file)
+
+            # Compute PEM results
+            print(">> PEM calculation")
+            pressure = pem_input.pressure
+            mixed_mineral = pem_input.minerals.as_mixture
+            mixed_fluid = pem_input.fluids.as_mixture(pressure)
+            dry_rock_material = pem_input.dry_rock.material(mixed_mineral, pressure)
+            dense_packing = pem_input.dry_rock.nonporous(mixed_mineral)
+            saturated_rock = pem.material.fluid_substitution(
+                dry_rock_material,
+                dense_packing,
+                mixed_fluid,
+                pem_input.dry_rock.porosity,
+            )
+
+            # OUTPUTS
+            vp = saturated_rock.primary_velocity
+            vs = saturated_rock.secondary_velocity
+
+            # Compute dT properties (used for TWT properties)
+            dz = subset_data["dZ"]
+            dtpp = 2000.0 * dz / vp
+            dtss = 2000.0 * dz / vs
+            dtps = (dtpp + dtss) / 2.0
+
+            res = pd.DataFrame(
+                {
+                    "VP_" + date + RUN_ID: vp,
+                    "VS_" + date + RUN_ID: vs,
+                    "AI_" + date + RUN_ID: saturated_rock.acoustic_impedance,
+                    "SI_" + date + RUN_ID: saturated_rock.shear_impedance,
+                    "DENS_" + date + RUN_ID: saturated_rock.density,
+                    "VPVS_" + date + RUN_ID: vp / vs,
+                    "DTPP_" + date + RUN_ID: dtpp,
+                    "DTSS_" + date + RUN_ID: dtss,
+                    "DTPS_" + date + RUN_ID: dtps,
+                },
+                index=subset_data.index,
+            )
+
+            # Add PEM results to the inputs and merge it with "results"
+            subset_results = pd.concat([subset_data, res], axis=1)
+            results = pd.concat([results, subset_results])
+
+            # Delete csv file for subset data
+            remove(subset_data_file)
+
+        # Re-order the results in increasing indexes to match the data format
+        results.sort_index(inplace=True)
+
+        # drop columns already included (df_ecl), append the pem calculated
+        drop_columns = PROPS_STATIC + PROPS_DYNAMIC
+        dfs.append(results.drop(columns=drop_columns))
+
+    df_all = pd.concat(dfs, axis=1)
+    df_all.to_csv(PEM_CSV_RESULTS, index=False)
+
+    print("6", time.perf_counter() - t_start)
+
 
 
 if __name__ == "__main__":
-    run_pem()
+    run_pem(PEM_CONFIG_FILES)
     print("done.")
